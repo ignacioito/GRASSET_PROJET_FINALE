1f194002c4bab6453c5426746c83ca85f950490d
Vatea Mahuta <31545923+Pivo987@users.noreply.github.com>
2019-09-26 14:07:24 -0400

    c1

--- js/admin.js
+++ js/admin.js
@@ -1,7 +1,18 @@
-let ingredient = "";
-let weight = 0;
-let
-
-function add() {
-  ingredient = document.getElementById("foodToAdd").value;
+function addDay1() {
+  // Find selected meal
+  let ddown = document.getElementById("mealNumbSelection");
+  let mealNumber = ddown.options[ddown.selectedIndex].value;
+  console.log(mealNumber);
+  // Find selected tab ID / day
+  let activeTab = $(".tab-content").find(".active");
+  let dayNumber = activeTab.attr('id');
+  console.log(dayNumber);
+  // Add the ingredient
+  let ingredient = document.getElementById("ingName").value;
+  console.log(ingredient);
+  let ingToAdd = document.createElement("li");
+  ingToAdd.textContent = ingredient;
+  let ulID = dayNumber + mealNumber;
+  console.log(ulID);
+  document.getElementById(ulID).appendChild(ingToAdd);
 }
