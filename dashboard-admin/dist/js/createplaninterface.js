b4eba217e03ea3c3c571273f63155f0e63712c41
Vatea Mahuta <peave@hotmail.fr>
2019-10-15 14:23:34 -0400

    Update createplaninterface.js

--- dashboard-admin/dist/js/createplaninterface.js
+++ dashboard-admin/dist/js/createplaninterface.js
@@ -145,23 +145,23 @@ function addDay1() {
 
   //
 
-  // for (var i = 0; i < ingredients.length; i++) {
-  //   if (ingredient.toUpperCase() == ingredients[i].toUpperCase()) {
-  //     // Find selected meal
-  //     let ddown = document.getElementById("mealNumbSelection1");
-  //     let mealNumber = ddown.options[ddown.selectedIndex].value;
-  //     console.log(mealNumber);
-  //     // Assign selected day
-  //     let dayNumber = "day1";
-  //     // Add the ingredient
-  //     console.log(ingredient);
-  //     let ingToAdd = document.createElement("li");
-  //     ingToAdd.textContent = ingredient;
-  //     let ulID = dayNumber + mealNumber;
-  //     console.log(ulID);
-  //     document.getElementById(ulID).appendChild(ingToAdd);
-  //   }
-  // }
+  for (var i = 0; i < ingredients.length; i++) {
+    if (ingredient.toUpperCase() == ingredients[i].toUpperCase()) {
+      // Find selected meal
+      let ddown = document.getElementById("mealNumbSelection1");
+      let mealNumber = ddown.options[ddown.selectedIndex].value;
+      console.log(mealNumber);
+      // Assign selected day
+      let dayNumber = "day1";
+      // Add the ingredient
+      console.log(ingredient);
+      let ingToAdd = document.createElement("li");
+      ingToAdd.textContent = ingredient;
+      let ulID = dayNumber + mealNumber;
+      console.log(ulID);
+      document.getElementById(ulID).appendChild(ingToAdd);
+    }
+  }
 }
 
 // Day2
