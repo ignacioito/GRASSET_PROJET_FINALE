c71f014aeb923d56803e588eaa62a76bb3eeab85
Ignacio Ito <ignaciotrabalho@gmail.com>
2019-10-15 00:46:57 -0400

    commit logout
    add logout page and option to menu dash

--- dashboard-admin/parameters.php
+++ dashboard-admin/parameters.php
@@ -116,6 +116,16 @@
                 </p>
               </a>
             </li>
+
+            <!-- Logout -->
+            <li class="nav-item">
+              <a href="logout.php" class="nav-link">
+                <i class="nav-icon fas fa-sign-out-alt"></i>
+                <p>
+                  Logout
+                </p>
+              </a>
+            </li>
           </ul>
         </nav>
       </div>
@@ -137,6 +147,75 @@
       <!-- /.content-header -->
 
       <!-- Main content -->
+
+        <form class="container">
+          <form class="content-panel">
+            <form class="form-horizontal">
+              <fieldset class="fieldset">
+                <div class="form-group avatar">
+                  <figure class="figure col-md-2 col-sm-3 col-xs-12">
+                    <img class="img-rounded img-responsive" src="https://bootdey.com/img/Content/avatar/avatar1.png"
+                      alt="">
+                  </figure>
+                  <div class="form-inline col-md-10 col-sm-9 col-xs-12">
+                    <input type="file" class="file-uploader pull-left">
+                    <button type="submit" class="btn btn-sm btn-default-alt pull-left">Update Image</button>
+                  </div>
+                </div>
+                <div class="form-group">
+                  <label class="col-md-2 col-sm-3 col-xs-12 control-label">User Name</label>
+                  <div class="col-md-10 col-sm-9 col-xs-12">
+                    <input type="text" class="form-control" value="Rebecca">
+                  </div>
+                </div>
+
+                <div class="form-group">
+                  <label class="col-md-2 col-sm-3 col-xs-12 control-label">First Name</label>
+                  <div class="col-md-10 col-sm-9 col-xs-12">
+                    <input type="text" class="form-control" value="Rebecca">
+                  </div>
+                </div>
+                <div class="form-group">
+                  <label class="col-md-2 col-sm-3 col-xs-12 control-label">Last Name</label>
+                  <div class="col-md-10 col-sm-9 col-xs-12">
+                    <input type="text" class="form-control" value="Sanders">
+                  </div>
+                </div>
+              </fieldset>
+              <fieldset class="fieldset">
+                <h3 class="fieldset-title">Contact Info</h3>
+                <div class="form-group">
+                  <label class="col-md-2  col-sm-3 col-xs-12 control-label">Couriel</label>
+                  <div class="col-md-10 col-sm-9 col-xs-12">
+                    <input type="email" class="form-control" value="Rebecca@website.com">
+                  </div>
+                </div>
+                <div class="form-group">
+                  <label class="col-md-2  col-sm-3 col-xs-12 control-label">Telephone</label>
+                  <div class="col-md-10 col-sm-9 col-xs-12">
+                    <input type="text" class="form-control" value="(514)3460031">
+                  </div>
+                </div>
+                <div class="form-group">
+                  <label class="col-md-2  col-sm-3 col-xs-12 control-label">Adresse</label>
+                  <div class="col-md-10 col-sm-9 col-xs-12">
+                    <input type="url" class="form-control" value="50 rue Sherbrook">
+                  </div>
+                </div>
+              </fieldset>
+              <hr>
+              <div class="form-group">
+                <div class="col-md-10 col-sm-9 col-xs-12 col-md-push-2 col-sm-push-3 col-xs-push-0">
+                  <input class="btn btn-primary" type="submit" value="Update Profile">
+                </div>
+              </div>
+            </form>
+          </form>
+        </form>
+
+
+    <!-- /.content -->
+  </div>
 
       <!-- /.content -->
     </div>
--- dashboard-user/createtrainingform.php
+++ dashboard-user/createtrainingform.php
@@ -95,6 +95,15 @@
                                 </p>
                             </a>
                         </li>
+                        <!-- Logout -->
+                        <li class="nav-item">
+                        <a href="logout.php" class="nav-link">
+                        <i class="nav-icon fas fa-sign-out-alt"></i>
+                        <p>
+                            Logout
+                        </p>
+                        </a>
+                        </li>
                     </ul>
                 </nav>
             </div>
