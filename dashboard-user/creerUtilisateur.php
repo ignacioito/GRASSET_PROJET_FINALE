5818ecdb41cc3f902bf3a632b09a142aca8bdba9
Ialo Andreoli <43653962+ialo@users.noreply.github.com>
2019-10-25 18:05:57 -0400

    Final SextaFeira

--- dashboard-user/creerUtilisateur.php
+++ dashboard-user/creerUtilisateur.php
@@ -4,22 +4,46 @@ include ('../website/connexion.php');
 
 if ($_POST) {
 
-        $email = $_POST['email'];
-        $motDePass = $_POST['motDePass'];
-        
-        
-        $sql =  "INSERT INTO `utilisateurs` (`type`, `eMail`, `nom`, `motDePasse`) "
-          .  "VALUES (2, '$email', '$email', '$motDePass')";
+    $email = $_POST['email'];
+    $motDePass = $_POST['motDePass'];
+
+    $sql = "select * from utilisateurs where eMail = '$email';";
+
+    $resultado = mysqli_query($connexion, $sql);
+
+    $numRegistros = mysqli_num_rows($resultado);
+
+    if($numRegistros > 0 ){
+
+        $sql = "UPDATE utilisateurs
+                SET eMail = '$email', motDePasse = $motDePass "
+                . "WHERE eMail = '$email';";
 
         $resultado = mysqli_query($connexion, $sql);
         
-       echo($resultado);
-    
-    }
-    
-    $connexion->close();
+//        echo($sql);
+        
+//        echo 'Cheguei aqui';
+
+    } else {
+            $sql =  "INSERT INTO `utilisateurs` (`type`, `eMail`, `nom`, `motDePasse`) "
+              .  "VALUES (2, '$email', '$email', '$motDePass')";
+
+            $resultado = mysqli_query($connexion, $sql);
+
+            if ($resultado == 1) {
+              $msg = '<h1>Aliment crée avec sucess</h1>';
+            } else {
+              $msg = 'Error';
+            }
+        }
+
+    $connexion->close();            
+}
+  
+   
     
    header('Location: ../dashboard-admin/manageclient.php');
-#    $var = "<script>javascript:history.back(-1)</script>";
+    $var = "<script>javascript:history.back(-1)</script>";
 #    echo $var;
 ?>
